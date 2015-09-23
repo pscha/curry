b7134fbc5247aed3c7954d8c9f867f878005498b
Sascha Martensen <martensensascha@gmail.com>
2015-09-23 20:29:01 +0200

    created curry.sh

---
+++ curry.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# curry is a tool to manage configurations
+# 
+
+if [ "$1" = "new" ]; then
+	# create a new directory
+	mkdir -p $HOME/.curry/$2
+fi
+
+if [ "$1" = "add" ]; then
+	# copy the configurationfile into the .curry directory
+	#mkdir -p $HOME/.curry/$2/$(pwd)/
+	cp -R --parent $3 $HOME/.curry/$2
+fi
+
+if [ "$1" = "load" ]; then
+	# load all the configurations
+	sudo cp -Rf $HOME/.curry/$2/* /
+fi
+
+if [ "$1" = "list" ]; then
+	ls $HOME/.curry
+fi
+
